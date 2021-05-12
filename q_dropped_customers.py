42ab4d405dfbd485ed09020189e6abd88a128cc8
davidchu <davidchu@bawakayi.com>
2021-05-12 08:42:29 -0700

    Simulation configured to be similar to 100k queries per hour, with 5 minute mean service time (session length).  Varying num servers from 1k to 10k.  Most interetsed in pct. requests dropped (with minimal wait queue support).

--- q_dropped_customers.py
+++ q_dropped_customers.py
@@ -3,21 +3,22 @@ from MMSK.simulation import *
 import matplotlib.pyplot as plt
 import math
 
-lam = 1*1000  # arrivals per hour
-meanServiceTime = 15/60 # mean service time
+lam = 100*1000  # arrivals per hour
+meanServiceTime = 5/60 # mean service time
 mu = 1/meanServiceTime  # param of exponential distribution. 1/mu is mean service time
-#sList = range(1,8)
-sList = [i * 100 for i in range(1,6)] # num servers
+# sList = range(1,8)
+sList = [i * 1000 for i in range(1,11)] # num servers
 # k = 8  # max number of customers (in queue + being served by servers)
 waitQueueLen = 1 # set to at least 1 to avoid bug. replaces k
 x = []
 simLy = []
+simWy = []
+simLqy = []
+simWqy = []
 simHours = 2
 numCustomers = lam * 2
 #numCustomers = 2000
 numCustomersDroppedPct = []
-
-print(np.random.exponential(1/lam)*60)
 
 print(f'lam={lam}, meanServiceTime={meanServiceTime}, mu={mu}')
 
@@ -33,10 +34,16 @@ for i in sList:
     k = i+waitQueueLen
     quSim = queueSim(lam, mu, i, k, numCustomers)
     quSim.simRun()
-    simLy.append(quSim.avgWaitLen)
+    simLy.append(quSim.avgWaitLen) # The average number of customers in the system
+    simWy.append(quSim.avgWaitTime) # The average dwell time of a customer in the system
+    simLqy.append(quSim.avgWaitQuLen) # The average queuing length
+    simWqy.append(quSim.avgWaitQuTime) # The average waiting time of customers
     numCustomersDroppedPct.append(quSim.numCustomersDropped / numCustomers)
 
 print('SIM :x = ', x, ', avgWaitLen = ', simLy)
+print('SIM :x = ', x, ', avgWaitTime = ', simWy)
+print('SIM :x = ', x, ', avgWaitQuLen = ', simLqy)
+print('SIM :x = ', x, ', avgWaitQuTime = ', simWqy)
 print('SIM2 :x = ', x, ', dropped = ', numCustomersDroppedPct)
 
 plt.plot(x, numCustomersDroppedPct, 'bo-', label='pct. dropped')
